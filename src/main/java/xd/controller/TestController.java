8e93bbb10d1cb3d1e6a5921b066f678640c88f0f
mac <429033084@qq.com>
2019-03-15 21:19:01 +0800

    XD

--- src/main/java/xd/controller/TestController.java
+++ src/main/java/xd/controller/TestController.java
@@ -2,15 +2,29 @@ package xd.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xd.model.Cargo;
+import xd.repository.CargoRepository;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by mac on 2019/3/13.
  */
 @RestController
 public class TestController {
+@Resource
+    CargoRepository cargoRepository;
+
 
     @RequestMapping("/aaa")
     public String test(){
         return "hello!";
     }
+
+//    @RequestMapping("/bbb")
+//    public List<Cargo> test2(){
+//       List<Cargo> c= (List<Cargo>) cargoRepository.findAll();
+//        return c;
+//    }
 }
